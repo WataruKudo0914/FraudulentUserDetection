38104be4f0b68f42b1b7a5d2f8df9a61e9eee6f8
Ubuntu <ubuntu@ip-172-31-6-194.us-east-2.compute.internal>
2019-12-19 20:58:29 +0900

    parser のバグ修正

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -8,7 +8,7 @@ def get_parser():
     parser.add_argument('--data_name', type=str)
     parser.add_argument(
         '--exp4_rate_list', '-r',
-        type=lambda x: x.split(','),
+        type=lambda x: map(float, x.split(',')),
         default=[0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.15,
                  0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
     parser.add_argument(
