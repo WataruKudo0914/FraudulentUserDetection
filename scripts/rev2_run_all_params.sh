16bbd5d3f787abc4b3ff1685844d4408413dcb25
Wataru Kudo <wataru-kudo048@g.ecc.u-tokyo.ac.jp>
2019-12-29 22:25:04 +0900

    rev2ローカル用

--- scripts/rev2_run_all_params.sh
+++ scripts/rev2_run_all_params.sh
@@ -11,17 +11,16 @@ do
 			c4=0
 			while [ $c4 -le 2 ]
 			do
-				python -m src.models.rev2.run_rev2 --data-name $DATA --alpha1 $c1 --beta1 $c2\
-													 --gamma1 $c3 --gamma2 $c4&
+				python -m src.models.rev2.run_rev2 --data-name $DATA --alpha1 $c1 --beta1 $c2 --gamma1 $c3 --gamma2 $c4 &
 				echo $c1 $c2 $c3 $c4
 				(( c4+=1 ))
                 # wait;
 			done
 			(( c3+=1 ))
-			#wait;
+			wait;
 		done
 		(( c2+=1 ))
-		wait;
+		# wait;
 	done
 	(( c1+=1 ))
 	# wait;
