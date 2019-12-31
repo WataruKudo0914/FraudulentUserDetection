b6b7a808e44e637bc813cd1323c37450df439c33
Wataru Kudo <wataru-kudo048@g.ecc.u-tokyo.ac.jp>
2019-12-31 17:30:54 +0900

    epinions early

--- src/data/get_early.py
+++ src/data/get_early.py
@@ -64,6 +64,6 @@ if __name__ == '__main__':
     rates = [0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.15,
              0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     data_names = ['alpha', 'otc', 'amazon',
-                  'amazon_home', 'amazon_music', 'amazon_app']
+                  'amazon_home', 'amazon_music', 'amazon_app', 'epinions']
     for data_name, rate in itertools.product(data_names, rates):
         get_early_network(data_name, rate)
